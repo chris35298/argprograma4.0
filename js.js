0d08816a7d39d81daf9de66d49067d5361ef2e69
Christian Miranda <123092285+chris35298@users.noreply.github.com>
2023-01-19 13:05:16 -0300

    primer commit

---
+++ js.js
@@ -0,0 +1,9 @@
+
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = "Fecha de contratación: 01-03-2005 hasta 15-05-2018";
+  }
+
+
+  function myFunction2() {
+    document.getElementById("demo2").innerHTML = "Fecha de contratación: 01-03-2018 hasta continúo";
+  }
